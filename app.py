ca471601ba0f044c7b1f9631a1ab24f123e82faf
Stephane <stephane.ly@outlook.fr>
2020-03-27 19:18:18 +0000

    ajout de commentaires

--- app.py
+++ app.py
@@ -1,24 +1,25 @@
 from flask import Flask, request, jsonify
-import pprint
-import os
-import boto3
+import os, boto3
 
-UPLOAD_FOLDER = './downloads/'
-ALLOWED_EXTENSIONS = set(['txt', 'csv', 'pdf', 'jpg', 'png', 'jpeg', 'gif'])
+#l'API accepte les fichiers txt et csv
+ALLOWED_EXTENSIONS = set(['txt', 'csv'])
 
 app = Flask(__name__)
 
+#fonction qui verifie que le ficher est du bon format
 def allowed_file(filename):
         return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 @app.route('/conversion-json', methods=["POST", "GET"])
-def upload():
+def post():
     output = {}
+    #s'il n'y a pas de fichier, on renvoie un message d'erreur
     if 'file' not in request.files:
         resp = jsonify({'message' : 'No file in the request'})
         resp.status_code = 400
         return resp
     file = request.files['file']
+    #si le fichier et présent et du bon format, on le traite
     if file and allowed_file(file.filename):
         file2 = file
         output["1. name"] = file.filename
@@ -26,12 +27,11 @@ def upload():
         output["3. mimetype"] = file.mimetype
         output["4. extension"] = '.' in file.filename and file.filename.rsplit('.', 1)[1].lower()
         output["5. content"] = file.read().decode("utf-8", errors = 'ignore')
-        s3_client = boto3.resource('s3')
+        #enregistrement du fichier sur S3
+        s3_client = boto3.resource('s3') #enregistrement sur
         s3_client.Object('monbucketty', file.filename).put (Body = output["5. content"] )
-        #filefors3 = s3_client.Object('monbucketty', file.filename)
-        #filefors3.put(Body = output["5. content"])
-        #s3_client.upload_fileobj(file2, "monbucketty", file.filename)
         return output
+    #si le fichier est du mauvais format, on renvoie un message d'erreur
     else:
         resp = jsonify({'message' : 'Allowed file types are txt, csv, pdf, jpg, png, jpeg and gif'})
         resp.status_code = 400
